8a4ff18013b39741c886bbf0d756e629544dc9fb
Your Name <you@example.com>
2025-08-07 07:26:31 +0530

    new-rg for inventory app

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "new-rg"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "rg1" {
+  name     = "new-rg1-abhay"
+  location = "West Europe"
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "febef730-8d0e-4e87-b84a-70939f0ce0e0"
+}
